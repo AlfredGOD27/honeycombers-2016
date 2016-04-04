e2ffd1a88338445f7fcefc4fb3cf784a9166eed9
Cooper Dukes <me@cooperdukes.com>
2016-04-04 19:09:59 -0400

    nav wrapper

--- includes/structure/menu.php
+++ includes/structure/menu.php
@@ -85,73 +85,75 @@ function hc_do_nav() {
 						echo '<a href="' . get_permalink($top_item['item_id']) . '" class="menu-item-link">' . $label . '</a>';
 						echo '<button type="button" class="inactive-link"><span>' . $label . '</span> <i class="ico-arrow-down"></i> <i class="ico-arrow-up"></i></button>';
 
-						echo '<ul class="sub-menu clearfix">';
-							$column_count = count($top_item['columns']);
+						echo '<div class="sub-menu">';
+							echo '<ul class="clearfix">';
+								$column_count = count($top_item['columns']);
 
-							$i = 1;
-							foreach( $top_item['columns'] as $column ) {
-								// Column class
-								$column_class = '';
-								switch( $column_count ) {
-									case 2:
-										$column_class = 'one-half';
-										break;
-									case 3:
-										$column_class = 'one-third';
-										break;
-									case 4:
-										$column_class = 'one-fourth';
-										break;
-								}
-
-								// Type
-								echo 1 === $i % $column_count ? '<li class="menu-col menu-col-' . $column['type'] . ' clearfix first ' . $column_class . '">' : '<li class="menu-col menu-col-' . $column['type'] . ' clearfix ' . $column_class . '">';
-									switch( $column['type'] ) {
-										case 'links':
-											?>
-											<div class="one-half first left">
-												<i class="ico-<?php echo $column['icon']; ?>"></i>
-											</div>
-
-											<div class="one-half right">
-												<ul>
-													<?php
-													foreach( $column['item_ids'] as $item_id ) {
-														?>
-														<li>
-															<a href="<?php echo get_permalink($item_id); ?>"><?php echo get_the_title($item_id); ?></a>
-														</li>
-														<?php
-													}
-													?>
-												</ul>
-											</div>
-											<?php
+								$i = 1;
+								foreach( $top_item['columns'] as $column ) {
+									// Column class
+									$column_class = '';
+									switch( $column_count ) {
+										case 2:
+											$column_class = 'one-half';
 											break;
-										case 'post':
-											?>
-											<div class="one-third first">
-												<?php
-												if( !empty($column['label']) )
-													echo '<span>' . sanitize_text_field($column['label']) . '</span>';
-												?>
-
-												<a href="<?php echo get_permalink($column['item_id']); ?>"><?php echo get_the_title($column['item_id']); ?></a>
-											</div>
-
-											<div class="two-thirds">
-												<?php
-												if( has_post_thumbnail($column['item_id']) )
-													echo get_the_post_thumbnail($column['item_id'], 'archive-small', '', array('class' => 'async-load-image skip-image-on-mobile') );
-												?>
-											</div>
-											<?php
+										case 3:
+											$column_class = 'one-third';
+											break;
+										case 4:
+											$column_class = 'one-fourth';
 											break;
 									}
-								echo '</li>';
-								++$i;
-							}
-						echo '</ul>';
+
+									// Type
+									echo 1 === $i % $column_count ? '<li class="menu-col menu-col-' . $column['type'] . ' clearfix first ' . $column_class . '">' : '<li class="menu-col menu-col-' . $column['type'] . ' clearfix ' . $column_class . '">';
+										switch( $column['type'] ) {
+											case 'links':
+												?>
+												<div class="one-half first left">
+													<i class="ico-<?php echo $column['icon']; ?>"></i>
+												</div>
+
+												<div class="one-half right">
+													<ul>
+														<?php
+														foreach( $column['item_ids'] as $item_id ) {
+															?>
+															<li>
+																<a href="<?php echo get_permalink($item_id); ?>"><?php echo get_the_title($item_id); ?></a>
+															</li>
+															<?php
+														}
+														?>
+													</ul>
+												</div>
+												<?php
+												break;
+											case 'post':
+												?>
+												<div class="one-third first">
+													<?php
+													if( !empty($column['label']) )
+														echo '<span>' . sanitize_text_field($column['label']) . '</span>';
+													?>
+
+													<a href="<?php echo get_permalink($column['item_id']); ?>"><?php echo get_the_title($column['item_id']); ?></a>
+												</div>
+
+												<div class="two-thirds">
+													<?php
+													if( has_post_thumbnail($column['item_id']) )
+														echo get_the_post_thumbnail($column['item_id'], 'archive-small', '', array('class' => 'async-load-image skip-image-on-mobile') );
+													?>
+												</div>
+												<?php
+												break;
+										}
+									echo '</li>';
+									++$i;
+								}
+							echo '</ul>';
+						echo '</div>';
 					} else {
 						echo '<a href="' . get_permalink($top_item['item_id']) . '" class="menu-item-link">' . $label . '</a>';
 					}
