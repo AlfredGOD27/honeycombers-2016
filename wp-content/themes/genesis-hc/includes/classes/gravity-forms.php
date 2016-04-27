3aab1cf7df00ef278d6c3a4e0083d0b6062886ed
Cooper Dukes <me@cooperdukes.com>
2016-04-27 16:55:20 -0400

    add points

--- wp-content/themes/genesis-hc/includes/classes/gravity-forms.php
+++ wp-content/themes/genesis-hc/includes/classes/gravity-forms.php
@@ -9,6 +9,26 @@ class HC_Gravity_Forms {
 
 	}
 
+	public function add_points( $post_type, $user_id, $level, $quantity, $entry_id ) {
+
+		$key    = '_hc_' . $post_type . '_credits_' . $level;
+		$points = get_user_meta( $user_id, $key, true );
+		$points += absint($quantity);
+		update_user_meta( $user_id, $key, $points );
+
+		$data = array(
+			'post_type'          => $post_type,
+			'level'              => $level,
+			'target_user_id'     => $user_id,
+			'initiating_user_id' => $user_id,
+			'ref_id'             => $entry_id,
+			'amount'             => $quantity,
+		);
+
+		HC()->logs->add( $data );
+
+	}
+
 	public function fulfill_paypal_order( $entry, $feed, $transaction_id, $amount ) {
 
 		if( 'Paid' !== $entry['payment_status'] )
@@ -18,10 +38,10 @@ class HC_Gravity_Forms {
 
 		switch( $entry[1] ) {
 			case 'Events - Upgrade|200':
-				HC()->events->editor->add_points( $user_id, 'upgrade', 3, $entry['id'] );
+				HC()->events->editor->add_points( 'event', $user_id, 'upgrade', 3, $entry['id'] );
 				break;
 			case 'Events - Premium|1500':
-				HC()->events->editor->add_points( $user_id, 'premium', 5, $entry['id'] );
+				HC()->events->editor->add_points( 'event', $user_id, 'premium', 5, $entry['id'] );
 				break;
 		}
 
