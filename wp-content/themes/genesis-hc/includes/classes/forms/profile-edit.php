1b7c72794d9d87d4c12bc79ef684644c019fd5e3
Cooper Dukes <me@cooperdukes.com>
2016-05-10 14:53:36 -0400

    subscription lists

--- wp-content/themes/genesis-hc/includes/classes/forms/profile-edit.php
+++ wp-content/themes/genesis-hc/includes/classes/forms/profile-edit.php
@@ -87,14 +87,8 @@ class HC_Profile_Edit_Form extends HC_Form_Abstract {
 			'required'    => false,
 			'interests'   => array(
 				'bd8c8e73a4' => 'Bali',
-				'1b924312a1' => 'Bangkok',
-				'cf6431e0fc' => 'Hong Kong',
 				'3ff287c860' => 'Jakarta',
-				'262a76a060' => 'Kuala Lumpur',
-				'b406482699' => 'Melbourne',
-				'41f0c232da' => 'Shanghai',
 				'5f7798b95f' => 'Singapore',
-				'19924cdc98' => 'Sydney',
 			),
 			'classes' => array('first', 'one-third'),
 		);
