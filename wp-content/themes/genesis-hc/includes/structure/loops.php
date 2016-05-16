f84c59c8b11285909f414b7f68033c7a85618782
Cooper Dukes <me@cooperdukes.com>
2016-05-16 16:07:04 -0400

    breadcrumb titles

--- wp-content/themes/genesis-hc/includes/structure/loops.php
+++ wp-content/themes/genesis-hc/includes/structure/loops.php
@@ -11,10 +11,20 @@ add_action( 'genesis_before_loop', 'hc_do_breadcrumbs' );
  */
 function hc_do_breadcrumbs() {
 
+	if( !is_singular('post') )
+		return;
+
 	if( function_exists('yoast_breadcrumb') ) {
 		yoast_breadcrumb( '<p class="breadcrumbs">', '</p>' );
 	} else {
 		genesis_do_breadcrumbs();
 	}
+
+}
+
+add_filter( 'wp_seo_get_bc_title', 'hc_wp_seo_get_bc_title', 10, 2 );
+function hc_wp_seo_get_bc_title( $title, $post_id ) {
+
+	return HC()->entry->get_headline_title($post_id);
 
 }
