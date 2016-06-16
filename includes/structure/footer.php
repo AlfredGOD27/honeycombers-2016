c06505e079bd40b8611b340ca11f3117cae445bd
AlfredGOD27 <alfred.david@thehoneycombers.com>
2016-06-16 15:48:43 +0800

    removed takeover home footer

--- includes/structure/footer.php
+++ includes/structure/footer.php
@@ -173,35 +173,6 @@ function hc_ie_font_face_fix() {
 }
 
 function takeover_after_footer() {
-
-		if ( is_front_page() ) {	 ?>
-        	<?php 
-				// Takeover Ad
-				if( have_rows('_hc_takeover') ):
-					while ( have_rows('_hc_takeover') ) : the_row();
-					$bg_color = get_sub_field('background_color');
-					$image = get_sub_field('image');
-					$url = get_sub_field('url');
-
-				if ($image) {
-			?>
-				<div id="takeover" style="background: <?php echo $bg_color; ?>; text-align: center;"><a href="<?php echo $url ?>"><img src="<?php echo $image['url'] ?>"></a></div>
-                <style>
-					@media only screen and (max-width: 789px) {
-					#takeover {
-						margin-top: 0;
-    					margin-bottom: 0
-					}
-				</style>
-			<?php 
-				}
-					endwhile;
-				else :
-				endif;
-			?>
-        	
-    <?php
-		}
 			if ( is_page('calendar') ) {	 ?>
 					<?php 
 						// Takeover Ad
