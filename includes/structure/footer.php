0b33a2e405c8714c6ebe03126cbe50d2da8efcac
AlfredGOD27 <alfred.david@thehoneycombers.com>
2016-06-16 14:22:25 +0800

    footer takeover

--- includes/structure/footer.php
+++ includes/structure/footer.php
@@ -100,6 +100,36 @@ function hc_do_footer() {
 		?>
 		<p>&copy; Honeycombers Pte Ltd <?php echo date('Y'); ?> All Rights Reserved</p>
 	</div>
+    <?php
+		if ( is_front_page() ) {	 ?>
+        	<?php 
+				// Takeover Ad
+				if( have_rows('_hc_takeover') ):
+					while ( have_rows('_hc_takeover') ) : the_row();
+					$bg_color = get_sub_field('background_color');
+					$image = get_sub_field('image');
+					$url = get_sub_field('url');
+
+				if ($image) {
+			?>
+				<div id="takeover" style="background: <?php echo $bg_color; ?>; text-align: center;"><a href="<?php echo $url ?>"><img src="<?php echo $image['url'] ?>"></a></div>
+                <style>
+					@media only screen and (max-width: 789px) {
+					#takeover {
+						margin-top: -21px;
+    					margin-bottom: 24px;
+					}
+				</style>
+			<?php 
+				}
+					endwhile;
+				else :
+				endif;
+			?>
+        	
+    <?php
+		}
+	?>
 	<?php
 
 }
