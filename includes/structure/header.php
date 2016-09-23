1fadcfb226ab335c38cdd0ea6a23e320abb01a31
AlfredGOD27 <alfred.david@thehoneycombers.com>
2016-09-23 11:36:51 +0800

    GA

--- includes/structure/header.php
+++ includes/structure/header.php
@@ -367,14 +367,15 @@ function hc_ga_content_grouping() {
 	?>
     
     <script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 	
 	  ga('create', 'UA-38721717-1', 'auto');
-	  ga('send', 'pageview');
-	  
+	  setTimeout("ga('send','event','Profitable Engagement','time on page more than 1 minute')",60000);
+	  //ga('send', 'pageview');
+	 
 	</script>
     
     <script>
