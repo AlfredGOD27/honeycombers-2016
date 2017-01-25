361286b914b4dc9a886c20a694b431020b3bc4eb
AlfredGOD27 <alfred.david@thehoneycombers.com>
2017-01-25 10:55:29 +0800

    fontawesome share buttons

--- includes/classes/share.php
+++ includes/classes/share.php
@@ -4,7 +4,8 @@ if( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class HC_Share {
 	public function display( $post_id ) {
-
+		
+		/*
         $blog_id = get_current_blog_id();
 
         switch( $blog_id ) {
@@ -21,10 +22,17 @@ class HC_Share {
 
         if( empty($blog_id) )
         	return;
-
+		*/
 		?>
-		<button class="share-button btn btn-icon" id="postshare"><i class="ico-share"></i><span>Share</span></button>
-        <div class="pw-server-widget" data-id="<?php echo $data_id; ?>"></div>
+		<?php /* <button class="share-button btn btn-icon" id="postshare"><i class="ico-share"></i><span>Share</span></button> */ ?>
+        <?php /* <div class="pw-server-widget" data-id="<?php echo $data_id; ?>"></div> */ ?>
+        <div class="share-icons">
+        	<a class="whatsapp-share" href="whatsapp://send?text=<?php the_permalink(); ?>" data-action="share/whatsapp/share" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+            <a class="facebook-share" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a class="twitter-share" href="https://twitter.com/home?status=<?php the_permalink(); ?>" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a class="pinterest-share" href="https://pinterest.com/pin/create/button/?url=&media=<?php the_permalink(); ?>&description=" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+            <a class="email-share" href="mailto:?&body=<?php the_permalink(); ?>" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+        </div>
 		<?php
 
 	}
