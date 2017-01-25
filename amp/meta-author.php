eab67ed4bcfc08734ec7a6d7dd2fe5108b60930f
AlfredGOD27 <alfred.david@thehoneycombers.com>
2017-01-25 10:29:06 +0800

    AMP author

--- amp/meta-author.php
+++ amp/meta-author.php
@@ -1,3 +1,4 @@
 <div class="amp-wp-meta amp-wp-byline">
-	<span class="amp-wp-author author vcard">by Dinesh Ajith</span>
+	<?php $post_author = $this->get( 'post_author' ); ?>
+	<span class="amp-wp-author author vcard"><?php echo esc_html( $post_author->display_name ); ?></span>
 </div>
